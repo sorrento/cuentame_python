c2d4180af9b8b9daf08deb3ad3a2dde40fb86eb9
sorrento <milenko@halat.cl>
2023-07-11 18:29:34 +0200

    Delete secret_keys.py
    others

--- secret_keys.py
+++
@@ -1,7 +0,0 @@
-# Configuration for Parse API
-X_PARSE_APPLICATION_ID = 'osZTTBy5RTLsYv1I9Fgma8LfYnU2vn307cg82jDu'
-X_PARSE_REST_API_KEY = 'IgXyiDNjH71kImV2LUPw5Jg1BOFBt812m7pkVpo9'
-OPENAI_API_KEY = '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'
-# DEBUG = False  # Set this to False to start Tweeting live
-# ODDS = 8
-
